88129aa54affb03c3f3ec5ddbe8f69039f71cd96
gaurish <33gaurishpatil@gmail.com>
2026-04-27 13:22:00 +0530

    c8

--- src/components/ImageBanner.tsx
+++ src/components/ImageBanner.tsx
@@ -3,29 +3,39 @@ import { AnimatePresence, motion } from "framer-motion";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import React, { useEffect, useState } from "react";
 
-const bannerImages = [
-  {
-    id: 1,
-    imgMob:
-      "https://www.taneira.com/dw/image/v2/BKMH_PRD/on/demandware.static/-/Sites-Taneira-Library/default/dwf7ac20c1/HomePage/Banners/best-buy/Mob_Banner_Saree.jpg",
-    imgDesk:
-      "https://www.taneira.com/dw/image/v2/BKMH_PRD/on/demandware.static/-/Sites-Taneira-Library/default/dw27e87c78/HomePage/Banners/best-buy/Web_banner_Saree.jpg",
-  },
-  {
-    id: 2,
-    imgMob:
-      "https://www.taneira.com/dw/image/v2/BKMH_PRD/on/demandware.static/-/Sites-Taneira-Library/default/dw9e93fd14/HomePage/Banners/best-buy/Mob_Banner_Kurta.jpg",
-    imgDesk:
-      "https://www.taneira.com/dw/image/v2/BKMH_PRD/on/demandware.static/-/Sites-Taneira-Library/default/dw9e93fd14/HomePage/Banners/best-buy/Web_Banner_Kurta.jpg",
-  },
-  {
-    id: 3,
-    imgMob:
-      "https://www.taneira.com/dw/image/v2/BKMH_PRD/on/demandware.static/-/Sites-Taneira-Library/default/dwd61f58f6/HomePage/Banners/best-buy/Mob_Banner_USKD.jpg",
-    imgDesk:
-      "https://www.taneira.com/dw/image/v2/BKMH_PRD/on/demandware.static/-/Sites-Taneira-Library/default/dwd61f58f6/HomePage/Banners/best-buy/Web_Banner_USKD.jpg",
-  },
-];
+// const bannerImages = [
+//   {
+//     id: 1,
+//     imgMob:
+//       "https://www.taneira.com/dw/image/v2/BKMH_PRD/on/demandware.static/-/Sites-Taneira-Library/default/dwf7ac20c1/HomePage/Banners/best-buy/Mob_Banner_Saree.jpg",
+//     imgDesk:
+//       "https://www.taneira.com/dw/image/v2/BKMH_PRD/on/demandware.static/-/Sites-Taneira-Library/default/dw27e87c78/HomePage/Banners/best-buy/Web_banner_Saree.jpg",
+//   },
+//   {
+//     id: 2,
+//     imgMob:
+//       "https://www.taneira.com/dw/image/v2/BKMH_PRD/on/demandware.static/-/Sites-Taneira-Library/default/dw9e93fd14/HomePage/Banners/best-buy/Mob_Banner_Kurta.jpg",
+//     imgDesk:
+//       "https://www.taneira.com/dw/image/v2/BKMH_PRD/on/demandware.static/-/Sites-Taneira-Library/default/dw9e93fd14/HomePage/Banners/best-buy/Web_Banner_Kurta.jpg",
+//   },
+//   {
+//     id: 3,
+//     imgMob:
+//       "https://www.taneira.com/dw/image/v2/BKMH_PRD/on/demandware.static/-/Sites-Taneira-Library/default/dwd61f58f6/HomePage/Banners/best-buy/Mob_Banner_USKD.jpg",
+//     imgDesk:""
+//       // "https://www.taneira.com/dw/image/v2/BKMH_PRD/on/demandware.static/-/Sites-Taneira-Library/default/dwd61f58f6/HomePage/Banners/best-buy/Web_Banner_USKD.jpg",
+//   },
+// ];
+
+
+const Images = [
+  { id: 1, src: "/banners/1.webp" },
+  { id: 2, src: "/banners/2.webp" },
+  { id: 3, src: "/banners/3.jpg" },
+  { id: 4, src: "/banners/4.webp" },
+  { id: 5, src: "/banners/5.png" },
+  { id: 6, src: "/banners/6.png" },
+]
 
 const ImageBanner = () => {
   const [current, setCurrent] = useState(0);
@@ -44,21 +54,23 @@ const ImageBanner = () => {
   useEffect(() => {
     if (paused) return;
     const interval = setInterval(() => {
-      setCurrent((prev) => (prev === bannerImages.length - 1 ? 0 : prev + 1));
+      // setCurrent((prev) => (prev === bannerImages.length - 1 ? 0 : prev + 1));
+      setCurrent((prev) => (prev === Images.length - 1 ? 0 : prev + 1));
     }, 5000);
     return () => clearInterval(interval);
   }, [paused]);
 
   return (
     <section
-      className="relative w-full mt-2 md:mt-2 min-h-[500px]  max-h-[600px] "
+      className="relative w-full mt-2 md:mt-2 min-h-125  max-h-150 "
       onMouseEnter={() => setPaused(true)}
       onMouseLeave={() => setPaused(false)}
     >
       <AnimatePresence mode="wait">
         <motion.img
           key={current}
-          src={isMobile ? bannerImages[current].imgMob : bannerImages[current].imgDesk}
+          // src={isMobile ? bannerImages[current].imgMob : bannerImages[current].imgDesk}
+          src={Images[current].src}
           alt={`Banner ${current + 1}`}
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
@@ -73,7 +85,8 @@ const ImageBanner = () => {
         aria-label="Previous Banner"
         onClick={() =>
           setCurrent((prev) =>
-            prev <= 0 ? bannerImages.length - 1 : prev - 1
+            // prev <= 0 ? bannerImages.length - 1 : prev - 1
+            prev <= 0 ? Images.length - 1 : prev - 1
           )
         }
         className="absolute left-4 top-1/2 -translate-y-1/2 bg-white/30 hover:bg-white/70 text-gray-900 p-2 rounded-full transition z-10"
@@ -86,7 +99,8 @@ const ImageBanner = () => {
         aria-label="Next Banner"
         onClick={() =>
           setCurrent((prev) =>
-            prev === bannerImages.length - 1 ? 0 : prev + 1
+            // prev === bannerImages.length - 1 ? 0 : prev + 1
+            prev === Images.length - 1 ? 0 : prev + 1
           )
         }
         className="absolute right-4 top-1/2 -translate-y-1/2 bg-white/30 hover:bg-white/70 text-gray-900 p-2 rounded-full transition z-10"
@@ -96,16 +110,17 @@ const ImageBanner = () => {
 
       {/* Dots */}
       <div className="absolute bottom-4 left-1/2 -translate-x-1/2 flex gap-2 z-20">
-        {bannerImages.map((_, i) => (
-          <button
-            key={i}
-            aria-label={`Go to slide ${i + 1}`}
-            onClick={() => setCurrent(i)}
-            className={`h-2 rounded-full transition-all ${
-              i === current ? "bg-gray-900 w-5" : "bg-white/70 w-2"
-            }`}
-          />
-        ))}
+        {
+          // bannerImages.map((_, i) => (
+          Images.map((_, i) => (
+            <button
+              key={i}
+              aria-label={`Go to slide ${i + 1}`}
+              onClick={() => setCurrent(i)}
+              className={`h-2 rounded-full transition-all ${i === current ? "bg-gray-900 w-5" : "bg-white/70 w-2"
+                }`}
+            />
+          ))}
       </div>
     </section>
   );
